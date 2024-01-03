eae449caf43b9a92d026e00edcb5a8e4fb72d99c
Alex Honcharenko <a.goncharenko@ftr.group>
2024-01-03 13:30:23 +0200

    Initial Commit

---
+++ BookAudio/BookAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAudioApp.swift
+//  BookAudio
+//
+//  Created by alexdevios on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
